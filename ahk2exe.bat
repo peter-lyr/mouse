568f79f129f811fc1117f4f6669026c0cc46123a
peter-lyr <llydrp.ldp@gmail.com>
2024-05-23 14:38:20 +0800

    @echo off

--- ahk2exe.bat
+++ ahk2exe.bat
@@ -1,4 +1,4 @@
-REM  @echo off
+@echo off
 set exe=mouse.exe
 taskkill /f /im %exe%
 Ahk2Exe.exe /icon ..ico /base "C:\Program Files\AutoHotKey\v2\AutoHotkey32.exe" /in main.ahk /out %exe% /compress 1
