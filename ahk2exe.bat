7cc7ac1ee1168d94338783c4ca5acbf3dde3b1a6
peter-lyr <llydrp.ldp@gmail.com>
2024-05-23 10:16:59 +0800

    C:\Program Files\AutoHotKey\v2\AutoHotkey32.exe

--- ahk2exe.bat
+++ ahk2exe.bat
@@ -1,2 +1,2 @@
 @echo off
-Ahk2Exe.exe /icon ..ico /base AutoHotkey32.exe /in main.ahk /out mouse.exe /compress 1
+Ahk2Exe.exe /icon ..ico /base "C:\Program Files\AutoHotKey\v2\AutoHotkey32.exe" /in main.ahk /out mouse.exe /compress 1
