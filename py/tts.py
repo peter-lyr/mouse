6629655e2274713016f32fb55f039a10ac57fb92
peter-lyr <llydrp.ldp@gmail.com>
2025-02-16 10:38:37 +0800

    new_rate = 1.0 * 200

--- py/tts.py
+++ py/tts.py
@@ -25,6 +25,7 @@ temp = os.path.join(dp, "temp")
 
 py_tts_bat = os.path.join(temp, "py-tts.bat")
 
+
 # 定义文件监控事件处理类
 class FileChangeHandler(FileSystemEventHandler):
     def __init__(self, file_path):
@@ -38,7 +39,7 @@ class FileChangeHandler(FileSystemEventHandler):
                 if self.time and t - self.time < 0.05:
                     return
                 self.time = t
-                with open(self.file_path, 'r', encoding='utf-8') as file:
+                with open(self.file_path, "r", encoding="utf-8") as file:
                     content = file.read()
                 # 创建一个新进程来进行语音朗读
                 p = Process(target=self.read_content, args=(content,))
@@ -49,16 +50,20 @@ class FileChangeHandler(FileSystemEventHandler):
     def read_content(self, content):
         # 为每个进程创建独立的语音引擎实例
         engine = pyttsx3.init()
-        new_rate = 3.0 * 200  # 你可以根据需要调整这个值
-        engine.setProperty('rate', new_rate)
+        new_rate = 1.0 * 200  # 你可以根据需要调整这个值
+        engine.setProperty("rate", new_rate)
         # 朗读文件内容
-        if not content.strip(): # 没用到
-            content = content.replace(" ", "空格").replace("\t", "制表符").replace("\n", "回车符")
-        pattern = r'<!--.*?-->'
+        if not content.strip():  # 没用到
+            content = (
+                content.replace(" ", "空格")
+                .replace("\t", "制表符")
+                .replace("\n", "回车符")
+            )
+        pattern = r"<!--.*?-->"
         content = content.strip()
         print(content)
-        content = re.sub(pattern, '', content)
-        if content == '删除':
+        content = re.sub(pattern, "", content)
+        if content == "删除":
             return
         engine.say(content)
         engine.runAndWait()
@@ -82,7 +87,11 @@ if __name__ == "__main__":
         # 创建观察者对象
         observer = Observer()
         # 安排监控任务
-        observer.schedule(event_handler, path=os.path.dirname(os.path.abspath(file_path)), recursive=False)
+        observer.schedule(
+            event_handler,
+            path=os.path.dirname(os.path.abspath(file_path)),
+            recursive=False,
+        )
         # 启动观察者
         observer.start()
         try:
